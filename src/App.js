7a526ba7fef505a9266eadf10f76ec85bd19f0ed
Miles Di Peri <milesdiperi@gmail.com>
2021-05-31 18:48:49 -0700

    Week 4 commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
-import Directory from './components/DirectoryComponent';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
                 </div>
-                </Navbar>
-                <Directory />
-            </div>
+            </BrowserRouter>
         );
     }
 }
