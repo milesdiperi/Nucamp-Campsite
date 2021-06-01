2fe9ce81df069c9734ad450143cd697c7aa9d9f8
Miles Di Peri <milesdiperi@gmail.com>
2021-05-31 19:00:44 -0700

    React week 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
 class App extends Component {
